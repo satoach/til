45f05d3fe8acdcf5d47a552fb6b894137a45db7d
Akira Sato <satoach.net@example.com>
2018-04-01 23:34:26 +0900

    add the user define flag value

--- go/flag/flag.go
+++ go/flag/flag.go
@@ -3,12 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+type intSliceValue []int
+
+func (isv *intSliceValue) Set(str string) error {
+	for _, s := range strings.Split(str, ",") {
+		v, err := strconv.ParseInt(s, 0, 32)
+		if err != nil {
+			return err
+		}
+		*isv = append(*isv, int(v))
+	}
+	return nil
+}
+
+func (isv *intSliceValue) String() string {
+	return fmt.Sprintf("%v", *isv)
+}
 
 func main() {
 	var isTrue bool
-	flag.BoolVar(&isTrue, "v", false, "flag test")
+	var numbers intSliceValue
+
+	flag.BoolVar(&isTrue, "b", false, "flag test")
+	flag.Var(&numbers, "number", "number array test")
 	flag.Parse()
+
 	fmt.Println("isTrue:", isTrue)
-	return
+	fmt.Println(numbers)
 }
--- go/flag/test.sh
+++ go/flag/test.sh
@@ -2,5 +2,7 @@
 
 set -x
 go run flag.go
-go run flag.go -v
 go run flag.go -h
+go run flag.go -b
+go run flag.go -b=true --number 1,3,5
+go run flag.go --number 1,3,5,ng
